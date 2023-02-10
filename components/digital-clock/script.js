08dedb98690a53ada709192d98dc8b138c99e3b0
HaiBangNgo <sonbangmtp@gmail.com>
2023-02-10 11:15:01 +0100

    third commit

--- components/digital-clock/script.js
+++ components/digital-clock/script.js
@@ -10,7 +10,7 @@ function updateClock() {
     let h = new Date().getHours(); 
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = ""; 
+    let ampm = "AM"; 
     if(h>12){
         h = h-12;
         ampm = "PM";
